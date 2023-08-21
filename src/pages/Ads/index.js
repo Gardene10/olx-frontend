9196f9865a721a7cdacd24007fc5f620dd39e9e1
Gardene 10 <gardene.v.c@gmail.com>
2023-08-21 15:28:26 -0300

    pagination is working

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -1,37 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom" //useLocation to change the url
 
 import useAPI from "../../utils/olxAPI";
 import { PageArea } from "./styled"
 import { PageContainer } from "../../components/MainComponents"
 import AdItem from "../../components/partials/AdItem";
 
-let timer
+let timer //way to avoid doing too much requests
 
 const Page = () => {
     const api = useAPI()
     const navigate = useNavigate()
 
     const useQueryString = () => {
-        return new URLSearchParams( useLocation().search )
+        return new URLSearchParams( useLocation().search ) //catch any obj from the url
     }
     const query = useQueryString()
-
-    const [q, setQ] = useState( query.get("q") != null ? query.get("q") : "" )
+     //filters
+    const [q, setQ] = useState( query.get("q") != null ? query.get("q") : "" ) // fill out with the respective values
     const [cat, setCat] = useState( query.get("cat") != null ? query.get("cat") : "" )
     const [state, setState] = useState( query.get("state") != null ? query.get("state") : "" )
 
-    const [adsTotal, setAdsTotal] = useState(0)
+    const [adsTotal, setAdsTotal] = useState(0)  //total ads 
     const [stateList, setStateList] = useState([])
     const [categories, setCategories] = useState([])
     const [adList, setAdList] = useState([])
-    const [pageCount, setPageCount] = useState(0)
+    const [pageCount, setPageCount] = useState(0) //total ads page 
     const [currentPage, setCurrentPage] = useState(1)
 
-    const [resultOpacity, setResultOpacity] = useState(1)
-    const [loading, setLoading] = useState(true)
+    const [resultOpacity, setResultOpacity] = useState(1) //way to do an opacity in ads when the user is still typing
+    const [loading, setLoading] = useState(true)  //way to show if the page is loading
 
-    const getAdsList = async () => {
+    const getAdsList = async () => { //function used to do the request
         setLoading(true)
         let offset = (currentPage - 1) * 9
 
@@ -51,7 +51,7 @@ const Page = () => {
 
     useEffect(() => {           // sistema de paginação
         if(adList.length > 0) {
-            setPageCount(Math.ceil( adsTotal / adList.length ))
+            setPageCount(Math.ceil( adsTotal / adList.length )) 
         } else {
             setPageCount(0)
         }
@@ -59,14 +59,14 @@ const Page = () => {
 
     useEffect(() => {
         setResultOpacity(0.3)
-        getAdsList()
+        getAdsList() //executing
     }, [currentPage])
 
     useEffect(() => {
         var queryString = []
         if(q) {
-            queryString.push(`q=${q}`)
-        }
+            queryString.push(`q=${q}`) //creating a array with my options to use the join at the end
+        } 
         if(cat) {
             queryString.push(`cat=${cat}`)
         }
@@ -74,12 +74,12 @@ const Page = () => {
             queryString.push(`state=${state}`)
         }
 
-        navigate({          // o navigate tem a função de mudar a URL sem atualizar a tela
-            search: `?${queryString.join("&")}`     // o join junta os elementos do array, atribui os valores a uma string e separa pelo denominador. 
-        }) 
+        navigate({          // change the screen without update the window
+            search: `?${queryString.join("&")}` // o join junta os elementos do array, atribui os valores a uma string e separa pelo denominador. 
+        }) // the & is like we separe an url
 
-        if(timer) {                     // evita que uma nova requisição seja feita toda vez que uma letra for escrita; espera 2s após acabar de escrever 
-            clearTimeout(timer)
+        if(timer) {// evita que uma nova requisição seja feita toda vez que uma letra for escrita; espera 2s após acabar de escrever 
+            clearTimeout(timer) //waiting 2 sec to execute the request
         }
         timer = setTimeout(getAdsList, 1200)
         setResultOpacity(0.3)
@@ -134,7 +134,7 @@ const Page = () => {
                             {categories.map((i, k) =>
                                 <li 
                                 key={k} 
-                                className={cat == i.slug ? "categoryItem active" : "categoryItem"}
+                                className={cat === i.slug ? "categoryItem active" : "categoryItem"} //selecting part that was clicked
                                 onClick={() => setCat(i.slug)}
                                 >
                                     <img src={i.img} alt="" />
